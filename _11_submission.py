2e08e5c0ff2ba13fc4fc1098a1dd2c155780d95b
PeterPirog <peterpirogtf@gmail.com>
2020-06-27 15:41:26 +0200

    added if model variable exist condition

--- _11_submission.py
+++ _11_submission.py
@@ -1,8 +1,9 @@
 import numpy as np
+import tensorflow as tf
 from tensorflow.keras import models
 import os
 
-MODEL_Q = 0 # global variable to checking if model is defined
+MODEL_Q = 0  # global variable to checking if model is defined
 
 
 def agent(observation, configuration):
@@ -15,14 +16,16 @@ def agent(observation, configuration):
     if not state.shape == (1, rows, columns, 1):
         print("State array shape error, state shape =", state.shape)
 
-    # Load agent model from file
-    if MODEL_Q == 0:  #if model isn't defined
-        os.system("gsutil cp gs://bert-pl/ConnectX/model_action_predictor.h5 ./") #path for cloud model file
-        while not os.path.exists('./model_action_predictor.h5'):  #wait until file exist in local directory
+    if not os.path.exists('./model_action_predictor.h5'):
+        os.system("gsutil cp gs://bert-pl/ConnectX/model_action_predictor.h5 ./")
+        while not os.path.exists('./model_action_predictor.h5'):
             pass
-            #print('File exist=',os.path.exists('./model_action_predictor.h5'))
 
-    MODEL_Q = models.load_model('./model_action_predictor.h5')
+    if not isinstance(MODEL_Q, tf.keras.Model): #if model variable not exist
+        MODEL_Q = models.load_model('./model_action_predictor.h5')
+
+    # JSON TEST
+    # json_model=MODEL_Q.to_json()
 
     # predict Q-values for current state
     prediction = MODEL_Q.predict(state)[0]
@@ -31,6 +34,7 @@ def agent(observation, configuration):
     action = np.argmax(prediction)
     action = np.int16(action).item()  # converting numpy int to native python int
     return action
+
 
 # This function converts obserwation from form:
 # {'board': [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 2, 1, 0, 0, 2, 2, 1, 1, 2, 2, 2, 2, 1, 2, 2, 1, 1, 1, 2], 'mark': 1}
